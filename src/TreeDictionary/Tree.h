96a24ab21a5737b21b083abac7fcab0d8c3b7062
Aurélien <clieledp@gmail.com>
2020-12-03 19:42:58 +0100

    Documentation Doxygen

--- src/TreeDictionary/Tree.h
+++ src/TreeDictionary/Tree.h
@@ -53,21 +53,47 @@ ListTree* transform_list(List* list);
 Node* min_list(ListTree** list);
 
 /**
- * \brief Function that returns the minimum of a list of nodes. Example :
+ * \brief Deletes an item from a list based on its information. Example :
  * \code{.c}
- * Node* node_left = min_list(list);
+ * suppr_min_list(&((*list)->next), c, occ);
  * \endcode
- * \param ListTree** list, initial list of which we want to find and delete the minimum.
- * \return Node*, the minimum node found.
+ * \param ListTree** list, list in which we want to remove the item.
+ * \param char c, information on the item to be deleted.
+ * \param int occ, information on the item to be deleted.
+ * \return Nothing.
  */
 void suppr_min_list(ListTree** list, char c, int occ);
 
+/**
+ * \brief Simple function that gives the size of a list of nodes.
+ * \param ListTree* list, the list.
+ * \return int, the size of the list.
+ */
 int size_of_list(ListTree* list);
 
+/**
+ * \brief Function to create a node from two old nodes.
+ * \param ListTree** list, the initial list containing all nodes.
+ * \return Node*, the new node created.
+ */
 Node* create_double_node(ListTree** list);
 
+/**
+ * \brief Function used to insert a node in a list of nodes.
+ * \param ListTree** list, the initial list containing all nodes.
+ * \param Node* node, the node to insert.
+ * \return Nothing.
+ */
 void insert_node(ListTree** list, Node* node);
 
+/**
+ * \brief Huffman tree creation function. Example :
+ * \code{.c}
+ * Node* huffman_tree = create_huffman_tree(&list_tree);
+ * \endcode
+ * \param ListTree** list, list from which the Huffman tree is created.
+ * \return Node*, the new Huffman tree.
+ */
 Node* create_huffman_tree(ListTree** list);
 
 
@@ -76,8 +102,18 @@ Node* create_huffman_tree(ListTree** list);
 //-------------------------------------------------------------
 
 
+/**
+ * \brief Simple function for free a list.
+ * \param List* l, the list.
+ * \return Nothing.
+ */
 void free_list(List* l);
 
+/**
+ * \brief Simple function for free a node.
+ * \param Node* node, the node.
+ * \return Nothing.
+ */
 void free_node(Node* node);
 
 
