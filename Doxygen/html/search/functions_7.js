9b7c399036eac7e9c5c1a19a55e6917aacfabc21
Aurélien <clieledp@gmail.com>
2020-12-05 17:07:27 +0100

    Update documentation

--- Doxygen/html/search/functions_7.js
+++ Doxygen/html/search/functions_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['text2binary_55',['text2binary',['../_binary_translation_8h.html#aae857dc1ea0e1ae7079c78304da1a676',1,'BinaryTranslation.c']]],
+  ['text2binary_55',['text2binary',['../_binary_translation_8h.html#add1ee5b39086f00f3f61889f5c6339ed',1,'BinaryTranslation.c']]],
   ['transform_5flist_56',['transform_list',['../_tree_8h.html#abb5cc00ade1dffcf9c4ed39f7de30d3d',1,'Tree.c']]]
 ];
